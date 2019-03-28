2fbf5c2399ceec953de2e0000766c773002a2e48
Adele <adelecalvo@gmail.com>
2019-03-28 11:52:00 -0400

    help file with logo

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-
-const minimist = require('minimist')
+const minimist = require('minimist');
 
 module.exports = () => {
-  const args = minimist(process.argv.slice(2))
-  const cmd = args._[0]
-  
+  const args = minimist(process.argv.slice(2));
+  const cmd = args._[0] || 'help';
+  // console.log(process.argv);
 
   switch (cmd) {
     case 'mkdockerfile':
-      require('./cmds/mkDockerfile')()
-      break
+      require('./cmds/mkDockerfile')();
+      break;
+    case 'mkcompose':
+      require('./cmds/mkCompose')();
+      break;
+    case 'help':
+      require('./cmds/help')();
+      break;
     default:
-      console.error(`"${cmd}" is not a valid command!`)
-      break
+      console.error(
+        `"${cmd}" is not a valid command! Type in 'dockerize-it help' to see valid commands`,
+      );
+      break;
   }
-}
-
+};
